9ba0106c4e908e7be2202006ecf6f0978e8729b8
aren talb <aren.talb00@gmail.com>
2024-01-29 17:01:15 +0300

    Install react router dom and write general routing code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./styles/global.css";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return <p>hello world </p>;
+  return (
+    <Routes>
+      <Route />
+      <Route />
+    </Routes>
+  );
 }
 
 export default App;
