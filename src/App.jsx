f2c1b8cb08ae21fb76e8f120d8c77d154a5d8726
aren talb <aren.talb00@gmail.com>
2024-02-05 14:11:01 +0300

    Competed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import Star from "./Star.jsx";
+
 function App() {
-  return <p>hello world </p>;
+  return (
+    <div>
+      <Star maxRating={10} initialValue={3} />
+      <Star color={"#4861e5"} maxRating={4} initialValue={22} />
+      <Star color={"#8d0082"} />
+      <Star
+        color={"#ffe91a"}
+        maxRating={4}
+        messages={["bad", "good", "very good", "excelent"]}
+      />
+      <Star
+        color={"#2de6ff"}
+        maxRating={4}
+        messages={["😟", "👍", "😀", "👏"]}
+        onChoose={handler}
+      />
+    </div>
+  );
+}
+
+function handler(data) {
+  console.log(data);
 }
 
 export default App;
