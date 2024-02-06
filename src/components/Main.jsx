c988c23a2b7b9145cb00d2fdc2606301dc08a21d
aren talb <aren.talb00@gmail.com>
2024-02-06 21:08:37 +0300

    Install chartjs and design pie chart

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,12 @@
 import styles from "../App.module.css";
+import { MyPieChart } from "@/components/ui/MyPieChart.jsx";
 
 export function Main() {
   return (
     <main className={`${styles.glass}  col-span-2 glass rounded-lg p-4`}>
-      main
+      <div className={"h-60 flex justify-center"}>
+        <MyPieChart />
+      </div>
     </main>
   );
 }
---
+++ src/components/ui/MyPieChart.jsx
@@ -0,0 +1,23 @@
+import { ArcElement, Chart as ChartJS, Legend, Tooltip } from "chart.js";
+import { Pie } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const data = {
+  labels: ["1", "2", "3"],
+  datasets: [
+    {
+      data: [300, 50, 100],
+      backgroundColor: [
+        "rgb(255, 99, 132)",
+        "rgb(54, 162, 235)",
+        "rgb(255, 205, 86)",
+      ],
+      hoverOffset: 4,
+    },
+  ],
+};
+
+export function MyPieChart() {
+  return <Pie data={data} />;
+}
