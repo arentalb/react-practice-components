e0449fbd034d60da5d1c3694d3e17d072173daef
aren talb <aren.talb00@gmail.com>
2024-02-06 18:38:13 +0300

    Design animated background

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import styles from "./App.module.css";
+
 function App() {
-  return <p>hello world </p>;
+  return (
+    <section
+      className={`${styles.mainBackground}  h-screen w-screen flex justify-center items-center `}
+    >
+      <div className={"grid grid-cols-3 w-1/2 h-3/4 gap-4"}>
+        <nav className={`${styles.glass} col-span-1 glass rounded-lg p-4`}>
+          nav
+        </nav>
+        <main className={`${styles.glass}  col-span-2 glass rounded-lg p-4`}>
+          main
+        </main>
+      </div>
+    </section>
+  );
 }
 
 export default App;
