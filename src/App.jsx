0fbdc43550f8e97139743126346dab80cd0ce83e
aren talb <aren.talb00@gmail.com>
2024-02-22 13:14:24 +0300

    Define the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { Navigate, Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router-dom";
+import { ProductsPage } from "./pages/ProductsPage.jsx";
+import { CartPage } from "./pages/CartPage.jsx";
+import { ProductPage } from "./pages/ProductPage.jsx";
+
 function App() {
-  return <p>hello world </p>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Navigate to={"products"} />} />
+        <Route path={"/products"} element={<ProductsPage />} />
+        <Route path={"/product/:id"} element={<ProductPage />} />
+        <Route path={"/cart"} element={<CartPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
